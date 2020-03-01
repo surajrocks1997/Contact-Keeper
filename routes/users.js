cb942db3f808c39799972caf038c9464c9277eb1
surajrocks1997 <surajrocks1997@gmail.com>
2020-03-01 17:56:48 +0530

    validation done for the users

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,27 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const User = require("../models/User");
+const { check, validationResult } = require("express-validator");
 
-router.post('/', (req,res) => {
-    res.send('Regiser a User');
-});
+router.post(
+    "/",
+    [
+        check("name", "Please add name")
+            .not()
+            .isEmpty(),
+        check("email", "Please Include a VALID EMAIL").isEmail(),
+        check(
+            "password",
+            "Please Enter Password with 6 or more characters"
+        ).isLength({ min: 6 })
+    ],
+    (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+        res.send("Passed");
+    }
+);
 
 module.exports = router;
-
