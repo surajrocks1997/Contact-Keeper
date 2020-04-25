7d51edc0f960b56d83cc507c6ddd63beb25994ce
surajrocks1997 <surajrocks1997@gmail.com>
2020-04-25 17:57:35 +0530

    contact form component and addContact method added, not working with API just yet

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -1,5 +1,5 @@
 import React, { useReducer } from "react";
-import uuid from "uuid";
+import { v4 as uuid } from "uuid";
 
 import ContactContext from "./contactContext";
 import ContactReducer from "./contactReducer";
@@ -43,6 +43,10 @@ const ContactState = (props) => {
   const [state, dispatch] = useReducer(ContactReducer, initialState);
 
   //add contact
+  const addContact = (contact) => {
+    contact.id = uuid();
+    dispatch({ type: ADD_CONTACT, payload: contact });
+  };
 
   //delete contact
 
@@ -60,6 +64,7 @@ const ContactState = (props) => {
     <ContactContext.Provider
       value={{
         contacts: state.contacts,
+        addContact,
       }}
     >
       {props.children}
