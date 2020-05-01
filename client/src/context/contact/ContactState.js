69b3f6c5abfbf4a4ba1ccd5a7e0310c5a14b714e
surajrocks1997 <surajrocks1997@gmail.com>
2020-05-01 11:20:56 +0530

    get and clear contacts added, user contacts are being fetched and shown in UI

--- client/src/context/contact/ContactState.js
+++ client/src/context/contact/ContactState.js
@@ -4,6 +4,7 @@ import axios from "axios";
 import ContactContext from "./contactContext";
 import ContactReducer from "./contactReducer";
 import {
+  GET_CONTACTS,
   ADD_CONTACT,
   DELETE_CONTACT,
   SET_CURRENT,
@@ -12,17 +13,34 @@ import {
   FILTER_CONTACTS,
   CLEAR_FILTER,
   CONTACT_ERROR,
+  CLEAR_CONTACTS,
 } from "../types";
 
 const ContactState = (props) => {
   const initialState = {
-    contacts: [],
+    contacts: null,
     current: null,
     filtered: null,
     error: null,
   };
 
   const [state, dispatch] = useReducer(ContactReducer, initialState);
+
+  //get contacts
+  const getContacts = async () => {
+    try {
+      const res = await axios.get("/api/contacts");
+      dispatch({
+        type: GET_CONTACTS,
+        payload: res.data,
+      });
+    } catch (err) {
+      dispatch({
+        type: CONTACT_ERROR,
+        payload: err.response.msg,
+      });
+    }
+  };
 
   //add contact
   const addContact = async (contact) => {
@@ -49,6 +67,11 @@ const ContactState = (props) => {
   //delete contact
   const deleteContact = (id) => {
     dispatch({ type: DELETE_CONTACT, payload: id });
+  };
+
+  //clear contacts
+  const clearContacts = () => {
+    dispatch({ type: CLEAR_CONTACTS });
   };
 
   //set current contact
@@ -90,6 +113,8 @@ const ContactState = (props) => {
         clearCurrent,
         filterContacts,
         clearFilter,
+        getContacts,
+        clearContacts,
       }}
     >
       {props.children}
